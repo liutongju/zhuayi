707a5801291e875cc572c9203e21ce03049e0ca4
linadelian@gmail.com <linadelian@gmail.com@8596d3ae-f4fe-9b1b-b058-0586270597b0>
2011-11-24 06:28:39 +0000

    增加检查是否开启rewrite模块

--- trunk/plugins/db/config/db.config.php
+++ trunk/plugins/db/config/db.config.php
@@ -2,7 +2,7 @@
 return array(
 				'mysql_host_m'	 =>		'localhost',
 				'mysql_host_s'	 =>		'localhost',
-				'mysql_user'	 =>		'root',
+				'mysql_user'	 =>		'root2',
 				'mysql_pass'		 =>		'worinima',
 				'mysql_db'	 =>		'zhuayi-net',
 				'mysql_port'     =>		'3306',
